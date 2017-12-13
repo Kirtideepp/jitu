067998d42707277f12190975089ff0b564c3cab7
kirtideepp <kirtideep.m@gmail.com>
2017-12-13 18:40:16 +0530

    4th commit
    Signed-off-by: kirtideepp <kirtideep.m@gmail.com>

--- Practiceeeeee/src/praaaaa/Time.java
+++ Practiceeeeee/src/praaaaa/Time.java
@@ -6,7 +6,7 @@ public class Time {
 		// TODO Auto-generated method stub
       System.out.println("hello");
       
-      System.out.println("ghgfhgfv");
+    
       System.out.println("gfthfyfuyfuyfuy");
 	}
 
